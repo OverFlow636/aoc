ebc1a2ca98b474b34d99326e19c3e77e8de31dcf
Joey Jan <overflow636@gmail.com>
2023-12-22 11:58:55 -0500

    redirect

--- src/index.js
+++ src/index.js
@@ -3,7 +3,26 @@ const app = express()
 const port = 3001;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    const host = req.headers && req.headers.host || null;
+    console.log('in host: ' + host);
+    switch (host) {
+        case 'redirect.sparkcures.com':
+            switch (req.path) {
+                case '/test': return res.redirect(302, 'https://sparkcures.com?testRedirect=true');
+            }
+
+            break;
+        case 'msk.sparkcures.doctor':
+            return res.redirect(302, 'https://sparkcures.doctor/?demoUser=msk');
+
+        case 'sparkcures.co':
+            break;
+
+        case 'ntrktrials.com':
+            return res.redirect(302, 'https://ntrkers.sparkcures.com/?utm_source=' + (req.path !== '/' ? req.path.substr(1).toLowerCase() : host));
+
+    }
+    return res.redirect(302, 'https://sparkcures.com/?unknownRedirect=' + host + req.path);
 })
 
 app.listen(port, () => {
